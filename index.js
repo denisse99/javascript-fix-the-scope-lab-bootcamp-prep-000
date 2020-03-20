a815ec067f80027b0a07072a5eafdb0498a6457e
Denisse Urdaneta <denisseurdaneta1999@hotmail.com>
2020-03-20 11:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var animal = 'dog'
-
-function myAnimal() {
-  return animal
+var animal='dog'
+function myAnimal(){
+  return Animal
 }
 
 function yourAnimal() {
+return animal
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
 }
 
 function add2(n) {
@@ -17,8 +16,8 @@ function add2(n) {
   // Feel free to move things around!
   const two = 2
 }
-
-var funkyFunction = function() {
+var theFunk= 'FUNKY!';
+var theFunk = function() {
   return function() {
     return "FUNKY!"
   }
